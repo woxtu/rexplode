50c4f7adfc286b86a269993cba365c8b0c1eab23
woxtu <woxtup@gmail.com>
2021-12-03 22:29:16 +0900

    Explode dot

--- tests/tests.rs
+++ tests/tests.rs
@@ -18,3 +18,8 @@ fn test_literals() {
   assert_eq!(explode(r"\u0061"), Ok(vec!["a".to_string()]));
   assert_eq!(explode(r"\u{0061}"), Ok(vec!["a".to_string()]));
 }
+
+#[test]
+fn test_dot() {
+  assert_eq!(explode("."), Ok(vec![".".to_string()]));
+}
